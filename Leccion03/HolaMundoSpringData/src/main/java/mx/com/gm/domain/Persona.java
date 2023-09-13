6e0287bdf67c5042094684379acecdea646c8558
antoluduenabereziuk21 <antolube20@gmail.com>
2023-09-13 13:36:52 -0300

    modifating classPersona for use table On DB

--- Leccion03/HolaMundoSpringData/src/main/java/mx/com/gm/domain/Persona.java
+++ Leccion03/HolaMundoSpringData/src/main/java/mx/com/gm/domain/Persona.java
@@ -1,9 +1,21 @@
 package mx.com.gm.domain;
 
+
+import jakarta.persistence.*;
+import java.io.Serializable;
 import lombok.Data;
-/*Data viene de lombok y nos auto generara el jabaBins(Constructores,geters y seters ,etc)*/
+
 @Data
-public class Persona {
+@Entity
+//para salvar errores indicamos como es el nombre de la tabla en la DB
+@Table(name = "persona")
+
+public class Persona implements Serializable{
+    
+    private static final long serialVersionUID= 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPersona;
     private String nombre;
     private String apellido;
     private String email;
