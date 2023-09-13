9281744465fcb9c293a40aed5f6aa8fb52559b54
antoluduenabereziuk21 <antolube20@gmail.com>
2023-09-13 11:29:02 -0300

    implementes if and each on thymeleaf

--- Leccion02/holaMundoThymeleaf/src/main/java/mx/com/gm/ControladorInicio.java
+++ Leccion02/holaMundoThymeleaf/src/main/java/mx/com/gm/ControladorInicio.java
@@ -1,5 +1,8 @@
 package mx.com.gm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import mx.com.gm.domain.Persona;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,14 +24,36 @@ public class ControladorInicio {
         var persona = new Persona();
         persona.setNombre("juan");
         persona.setApellido("perez");
-        persona.setEmail("juan@juan.com");
+        persona.setEmail("juan@mail.com");
         persona.setTelefono("35124546");
+        
+        var persona2 = new Persona();
+        persona2.setNombre("karla");
+        persona2.setApellido("gomez");
+        persona2.setEmail("kgomez@mail.com");
+        persona2.setTelefono("3512657812");
+        
+        //List<Persona> personas = new ArrayList();
+        //personas.add(persona);
+        //personas.add(persona2);
+        
+        //podemos utilzar var para la inferencia de tipos
+        //var personas = new ArrayList();
+        //personas.add(persona);
+        //personas.add(persona2);
+        
+        //tambien podemos utilizar esta anotacion tambien
+        var personas = Arrays.asList(persona,persona2);
+        //var personas = Arrays.asList();
+        
+        
         
         log.info("ejecutando el controlador Spring MVC");
         
         model.addAttribute("mensaje",mensaje);
         model.addAttribute("saludo", saludo);
-        model.addAttribute("persona", persona);
+        //model.addAttribute("persona", persona); compartiremos el array
+        model.addAttribute("personas", personas);
         return "index";/*por defualt thymeleaf crea archivos html*/
     }
 }
